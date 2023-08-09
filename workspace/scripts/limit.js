f40238fcf131943ec0cdd0157d3f4ff3f5232caa
thibault rko <hercod.thibault@gmail.com>
2023-08-09 15:54:29 +0200

    ajout limit, projection, sort

---
+++ workspace/scripts/limit.js
@@ -0,0 +1,9 @@
+let db = connect("mongodb://root:test123*@localhost:27017");
+// use technocite;
+db = db.getSiblingDB("sample_mflix");
+
+// récupérer max 5 films
+const movies = db.movies.find().limit(5);
+
+// on affiche le nombre de films récupérés
+console.log(movies.toArray().length);
